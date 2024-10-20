32c421b56abaa88510cf76cbdfcf3d64fee02ec6
ramonzeraa <ramoncandido64@gmail.com>
2024-10-20 15:08:46 -0300

    Criação das tabelas para usuários e itens de locação

---
+++ app/models.py
@@ -0,0 +1,17 @@
+# app/models.py
+from app import db
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    telefone = db.Column(db.String(20), nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
+
+class Artefato(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    categoria = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.Text, nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+    disponibilidade = db.Column(db.Boolean, default=True)
